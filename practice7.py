e6c9c4f4d5f73a70e5d2d14298914dd24b8a6245
Nitish622003 <97962018+Nitish622003@users.noreply.github.com>
2023-10-18 17:25:29 +0530

    Update practice7.py

--- practice7.py
+++ practice7.py
@@ -1,6 +1,6 @@
 num = int(input('Enter a number'))
 last_digit = num % 10
-if last_digit % 3 != 0:
-    print('its is not dividible by 3')
+if last_digit % 3 == 0:
+    print('its is  dividible by 3')
 else:
-    print('it is divisible by 3')
+    print('it is not divisible by 3')
